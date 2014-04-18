c0466ed8d32b0d6c73366d27bf7c8060ecbdf144
edan <edanl@outlook.com>
2014-04-17 20:14:27 -0400

    Initial mult-thread working

--- Anagram/Anagram/ViewController.m
+++ Anagram/Anagram/ViewController.m
@@ -12,17 +12,13 @@
 
 @interface ViewController ()
 
-@property (strong, nonatomic) NSMutableArray *potentialWords;
-//@property (strong, nonatomic) NSMutableSet *potentialWordsSet;
-
 @property DataStore *dataStore;
 
-
-//@property (strong, nonatomic) WordList *wordList;
-
-
+@property (strong, nonatomic) NSMutableArray *potentialWords;
 @property (weak, nonatomic) IBOutlet UITextField *letterInputs;
 @property (weak, nonatomic) IBOutlet UITextView *listOfWords;
+@property (strong, nonatomic) NSOperationQueue *queue;
+
 - (IBAction)activateLetterSortButton:(UIButton *)sender;
 
 
@@ -36,83 +32,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    //self.wordList = [[WordList alloc] init];
     
     self.dataStore = [DataStore sharedDataStore];
     
     self.letterInputs.delegate = self;
     [self.letterInputs becomeFirstResponder];
+    
+    self.queue = [[NSOperationQueue alloc] init];
+    //self.queue.maxConcurrentOperationCount=1;
+
 }
 
 
 
 
 
--(NSMutableArray *) recursiveLetterMixer: (NSMutableArray *) randomLetters
-{
-    
-    if([randomLetters count] > 2)
-    {
-        NSMutableArray *arrayToReturn = [[NSMutableArray alloc] init];
-        
-        for (NSInteger i = 0; i<[randomLetters count]; i++)
-         {
-           // NSLog(@"At i = %d, buildingBlock = %@", i, randomLetters);
-
-            NSMutableArray *buildingArray = [[NSMutableArray alloc] initWithArray:randomLetters copyItems:YES];
-            //NSLog(@"At i = %d, buildingBlock = %@", i, tempArray);
-
-            NSString *offBreak = [buildingArray objectAtIndex:i];
-            [buildingArray removeObjectAtIndex:i];
-            
-            NSMutableArray *tempArray = [[NSMutableArray alloc] init];
-            tempArray = [self recursiveLetterMixer:buildingArray];
-            
-            
-            for (NSInteger j = 0; j<[tempArray count]; j++){
-            //    NSLog(@"At j = %d, buildingArray = %@", j, buildingArray);
-                
-                NSString *tempString = [tempArray objectAtIndex:j];
-                NSString *newString = [offBreak stringByAppendingString:tempString];
-                
-                [arrayToReturn insertObject:newString atIndex:j];
-                //NSLog(@"At j= %d, offbreak = %@, arrayToReturn = %@", j, offBreak, arrayToReturn);
-            }
-         //  NSLog(@"Returning = %@", arrayToReturn);
-
-        }
-        //NSLog(@"Returning after while = %@", arrayToReturn);
-
-        return arrayToReturn;
-        
-    }
-    else if([randomLetters count] == 2) {
-        //NSLog(@"The 2s");
-
-        NSString *letter1 = [randomLetters objectAtIndex:0];
-        NSString *letter2 = [randomLetters objectAtIndex:1];
-        NSString *string1 = [letter1 stringByAppendingString:letter2];
-        NSString *string2 = [letter2 stringByAppendingString:letter1];
-        //[recurredBuildingBlock removeObject:buildingString];
-        
-        NSMutableArray *foundationArray = [[NSMutableArray alloc] init];
-        [foundationArray addObject:string1];
-        [foundationArray addObject:string2];
-        
-        return foundationArray;
-    }
-    else
-    {
-        return randomLetters;
-    }
-    
-    return randomLetters;
-}
-
-
-
-#pragma mark -text field delegate
+#pragma mark User Input Areas
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
@@ -127,14 +62,18 @@
 }
 
 
+#pragma mark Make it Work
+
 -(void) inputToWords
 {
-    NSMutableArray *characterInputReceived = [self turnInputIntoArray];
-    self.potentialWords = [self recursiveLetterMixer:characterInputReceived];
-    NSLog(@"All letter combinations: %@", self.potentialWords);
-    [self findingPossibleWords];
+
     
-    
+    [self.queue addOperationWithBlock:^{
+        NSMutableArray *characterInputReceived = [self turnInputIntoArray];
+        self.potentialWords = [self recursiveLetterMixer:characterInputReceived];
+        NSLog(@"All letter combinations: %@", self.potentialWords);
+        [self findingPossibleWords];
+    }];
 }
 
 
@@ -146,7 +85,6 @@
     for (NSInteger i=0;i<[inputFromUser length];i++) {
         unichar character;
         character = [inputFromUser  characterAtIndex:i];
-        //NSLog(@"Processing charachter %c",character);
         
         NSString *letter = [NSString stringWithFormat:@"%c", character];
         [characterInput addObject:letter];
@@ -159,30 +97,97 @@
 
 -(void)findingPossibleWords
 {
-    NSLog(@"Real words: ");
-    NSMutableArray *realWords = [[NSMutableArray alloc] init];
+    NSLog(@"Possible words: ");
     for (NSString *potentialWord in self.potentialWords)
     {
         if ([self.dataStore isDictionaryWord:potentialWord]) {
-        //if ([self isDictionaryWord:potentialWord]) {
             NSLog(@"%@", potentialWord);
-             [realWords addObject:potentialWord];
-
-            dispatch_async(dispatch_get_main_queue(), ^{
-                self.listOfWords.text = [realWords componentsJoinedByString:@"\n"];
-            });
+            [self writeWordToView:potentialWord];
         }
     }
-
 }
 
--(BOOL)isDictionaryWord:(NSString*) word {
-    UITextChecker *checker = [[UITextChecker alloc] init];
-    NSLocale *currentLocale = [NSLocale currentLocale];
-    NSString *currentLanguage = [currentLocale objectForKey:NSLocaleLanguageCode];
-    NSRange searchRange = NSMakeRange(0, [word length]);
-    NSRange misspelledRange = [checker rangeOfMisspelledWordInString:[word lowercaseString] range: searchRange startingAt:0 wrap:NO language: currentLanguage ];
-    return misspelledRange.location == NSNotFound;
+
+-(void)writeWordToView: (NSString *) word
+{
+    NSMutableArray *realWords = [[NSMutableArray alloc] init];
+    [realWords addObject:word];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.listOfWords.text = [realWords componentsJoinedByString:@"\n"];
+    });
+}
+
+#pragma mark Permutation Generation Recursion
+
+
+-(NSMutableArray *) recursiveLetterMixer: (NSMutableArray *) randomLetters
+{
+    
+    if([randomLetters count] > 2)
+    {
+        NSMutableArray *arrayToReturn = [[NSMutableArray alloc] init];
+        
+        for (NSInteger i = 0; i<[randomLetters count]; i++)
+        {
+            // NSLog(@"At i = %d, buildingBlock = %@", i, randomLetters);
+            
+            NSMutableArray *buildingArray = [[NSMutableArray alloc] initWithArray:randomLetters copyItems:YES];
+            //NSLog(@"At i = %d, buildingBlock = %@", i, tempArray);
+            
+            NSString *offBreak = [buildingArray objectAtIndex:i];
+            [buildingArray removeObjectAtIndex:i];
+            
+            NSMutableArray *tempArray = [[NSMutableArray alloc] init];
+            tempArray = [self recursiveLetterMixer:buildingArray];
+            
+            
+            for (NSInteger j = 0; j<[tempArray count]; j++){
+                //    NSLog(@"At j = %d, buildingArray = %@", j, buildingArray);
+                
+                NSString *tempString = [tempArray objectAtIndex:j];
+                NSString *newString = [offBreak stringByAppendingString:tempString];
+                
+                if ([self.dataStore isDictionaryWord:newString]) {
+                    NSLog(@"Yes to word: %@", newString);
+                }
+                [arrayToReturn insertObject:newString atIndex:j];
+                //NSLog(@"At j= %d, offbreak = %@, arrayToReturn = %@", j, offBreak, arrayToReturn);
+            }
+            //  NSLog(@"Returning = %@", arrayToReturn);
+            
+        }
+        //NSLog(@"Returning after while = %@", arrayToReturn);
+        
+        return arrayToReturn;
+        
+    }
+    else if([randomLetters count] == 2) {
+        //NSLog(@"The 2s");
+        
+        NSString *letter1 = [randomLetters objectAtIndex:0];
+        NSString *letter2 = [randomLetters objectAtIndex:1];
+        NSString *string1 = [letter1 stringByAppendingString:letter2];
+        NSString *string2 = [letter2 stringByAppendingString:letter1];
+        //[recurredBuildingBlock removeObject:buildingString];
+        
+        NSMutableArray *foundationArray = [[NSMutableArray alloc] init];
+        [foundationArray addObject:string1];
+        [foundationArray addObject:string2];
+        if ([self.dataStore isDictionaryWord:string1]) {
+            NSLog(@"Yes to word: %@", string1);
+        }
+        if ([self.dataStore isDictionaryWord:string2]) {
+            NSLog(@"Yes to word: %@", string2);
+        }
+        
+        return foundationArray;
+    }
+    else
+    {
+        return randomLetters;
+    }
+    
+    return randomLetters;
 }
 
 
